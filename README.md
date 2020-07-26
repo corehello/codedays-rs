3b255746ce2c364e08c96d662e42cd3bf3b5b977
qiwihui <qwh005007@gmail.com>
2020-07-26 11:29:38 +0800

    feat(dev): 开发时自动加载

--- README.md
+++ README.md
@@ -9,5 +9,13 @@ echo "DATABASE_URL=postgres://postgres:dayscode@localhost/codedays?sslmode=disab
 cargo install diesel_cli --features "postgres" --no-default-features
 diesel migration run
 
-cargo build
+cargo run
+```
+
+## 自动加载
+
+参见 https://actix.rs/docs/autoreload/ 进行配置
+
+```shell
+systemfd --no-pid -s http::3000 -- cargo watch -x run
 ```
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod schema;
 mod subscriber;
 
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
@@ -13,14 +14,20 @@ async fn index() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let mut listenfd = ListenFd::from_env();
     std::env::set_var("RUST_LOG", "actix_web=debug");
-    HttpServer::new(|| {
+    let mut server = HttpServer::new(|| {
         App::new()
             .service(web::resource("/").route(web::get().to(index)))
             .configure(kb::urls::configure)
             .configure(subscriber::urls::configure)
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    });
+    // 自动加载
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:8000")?
+    };
+
+    server.run().await
 }
